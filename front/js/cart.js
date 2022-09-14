482aa7ee7ac96d267f6dc03afbfbcd781f6ef17f
Dunvii <rsloik.arzur@gmail.com>
2022-09-14 15:22:15 -0300

    Working on total Price

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,16 +1,15 @@
 const KanapAPI = "http://localhost:3000/api/products/";
 let cartList = JSON.parse(localStorage.getItem('cart'));
-let lsCartList = JSON.parse(localStorage.getItem('cart'));
 let addArticle = document.querySelector('#cart__items');
 let totalQty = document.querySelector('#totalQuantity');
 let totalPrice = document.querySelector('#totalPrice');
-let firstName = document.querySelector('#firstName');
-let lastName = document.querySelector('#lastName');
-let address = document.querySelector('#address');
-let city = document.querySelector('#city');
-let email = document.querySelector('#email');
-let order = document.querySelector('#order');
-for(let product of cartList) {
+let allArticlePrice = [];
+
+if (cartList === null) {
+    updateShowCart();
+}
+else {
+    for(let product of cartList) {
     let KanapApiId = KanapAPI + product._id;
     fetch(KanapApiId)
     .then((res) => res.json())
@@ -42,19 +41,20 @@ for(let product of cartList) {
         addArticle.appendChild(newProduct);
         const buttonArticle = document.querySelector(`[data-id="${product._id}"][data-color="${product.color}"] .deleteItem`);
         const delArticle = document.querySelector(`[data-id="${product._id}"][data-color="${product.color}"]`);
+        const qtyInput = document.querySelector(`[data-id="${product._id}"][data-color="${product.color}"] .itemQuantity`);
+
         // Suppression d'un article au click
         buttonArticle.addEventListener("click", function() {
             delArticleInPage(delArticle);
             let returnCart = cartList.find(contentValue => contentValue._id === product._id && contentValue.color === product.color);
-            let article = lsCartList.filter(function(article) {
+            let article = cartList.filter(function(article) {
                 return !(article._id == returnCart._id && article.color == returnCart.color)
             });
             cartUpdate(article);
+            updateShowCart();
         });
+
         // Changement de la quantité d'un article
-        const qtyInput = document.querySelector(`[data-id="${product._id}"][data-color="${product.color}"] .itemQuantity`);
-        let totalArticlePrice = parseInt(qtyInput.value) * parseInt(infoProduct.price);
-        let showCart = parseInt(totalPrice.textContent);
         qtyInput.addEventListener('input', function () {
             if (qtyInput.value < 1 || qtyInput.value > 100) {
                 alert('Veuillez choisir une quantité comprise en 1 et 100.');
@@ -62,27 +62,42 @@ for(let product of cartList) {
             else {
                 let returnCart = cartList.find(contentValue => contentValue._id === product._id && contentValue.color === product.color);
                 returnCart.qty = parseInt(qtyInput.value);
-                console.log(cartList);
                 cartUpdate(cartList);
-                updateShowCart(totalArticlePrice, parseInt(qtyInput.value), showCart);
+                updateShowCart();
             }
         });
-        updateShowCart(totalArticlePrice, parseInt(qtyInput.value), showCart);
+        updateShowCart();
     });
+    }
 }
-function updateShowCart (totalArticlesPrice, totalArticleQty, showCart) {
-    if(isNaN(showCart)) {
-        let cartPrice = totalArticlesPrice;
-        let cartQty = totalArticleQty;
-        totalPrice.innerText = cartPrice;
-        totalQty.innerText = cartQty;
+
+
+function updateShowCart(){
+    if (cartList === null) {
+        totalQty.innerText = parseInt(0);
+        totalPrice.innerText = parseInt(0);
+        const newContent = document.createElement("h2");
+        newContent.setAttribute("style", "text-align: center");
+        newContent.textContent = "Panier vide";
+        addArticle.appendChild(newContent);
     }
     else {
-        let cartPrice = parseInt(totalPrice.innerText) + totalArticlesPrice;
-        let cartQty = parseInt(totalQty.innerText) + totalArticleQty;
-        totalPrice.innerText = cartPrice;
-        totalQty.innerText = cartQty;
+        totalQty.innerText = cartList.map(item => parseInt(item.qty)).reduce((compteur, valeur) => compteur + valeur);
+        for (product of cartList){
+            let KanapApiId = KanapAPI + product._id;
+            let articlePrice = parseInt(product.qty);
+            fetch(KanapApiId)
+            .then((res) => res.json())
+            .then ((infoProduct) => {
+                articlePrice = articlePrice * parseInt(infoProduct.price);
+                allArticlePrice.push(articlePrice);
+                console.log(allArticlePrice);
+            });
+            totalPrice.innerText = allArticlePrice.reduce((compteur, valeur) => compteur + valeur, 0);
+        }
+
     }
+    
 }
 
 // Suppression d'un article de la page
@@ -93,29 +108,5 @@ function delArticleInPage (article) {
 // Mise à jour du panier dans le LS
 function cartUpdate (product) {
     localStorage.setItem('cart', JSON.stringify(product));
-    let cartList = localStorage.getItem('cart');
+    cartList = JSON.parse(localStorage.getItem('cart'));
 }
-
-firstName.addEventListener('input', function() {
-
-});
-
-lastName.addEventListener('input', function() {
-
-});
-
-address.addEventListener('input', function() {
-
-});
-
-city.addEventListener('input', function() {
-
-});
-
-email.addEventListener('input', function() {
-
-});
-
-order.addEventListener('input', function() {
-
-});
